57e701f8a3e8d9b95080185c39422fd79e4f74de
Radomir <radomir.radionov26@gmail.com>
2023-11-10 00:09:01 +0300

    task6

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const imageWrapper = document.querySelector('.images');
 const loadMoreBtn = document.querySelector('.load-more');
+const searchInput = document.querySelector('.search input');
 
+// API key, paginations, searchTerm variables
 const apiKey = '9CLXIMXhpRXjM9CC6MhNX3fU3vyLvSyX0D48DlWq08c6jLyDfVrWb8jo';
 const perPage = 15;
 let currentPage = 1;
+let searchTerm = null;
 
 const generateHTML = (images) => {
   // Making li of all fetched images and adding them to the existing image wrapper
@@ -50,7 +53,26 @@ getImages(
 const loadMoreImages = () => {
   currentPage++; // Increment currentPage by 1
   let apiUrl = `https://api.pexels.com/v1/curated?page=${currentPage}&per_page=${perPage}`;
+  apiUrl = searchTerm
+    ? `https://api.pexels.com/v1/search?query=${searchTerm}&page=${currentPage}&per_page=${perPage}`
+    : apiUrl;
   getImages(apiUrl);
 };
 
+const loadSearchImages = (e) => {
+  //   // If the search input is empty, set the search term to null and return from here
+  if (e.target.value === '') return (searchTerm = null);
+  // If pressed key is Enter, update the current page, search term & call the getImages
+  if (e.key === 'Enter') {
+    console.log('Enter key pressed');
+    currentPage = 1;
+    searchTerm = e.target.value;
+    imageWrapper.innerHTML = '';
+    getImages(
+      `https://api.pexels.com/v1/search?query=${searchTerm}&page=1&per_page=${perPage}`
+    );
+  }
+};
+
 loadMoreBtn.addEventListener('click', loadMoreImages);
+searchInput.addEventListener('keyup', loadSearchImages);
